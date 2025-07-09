46c6ca2fea5fe320932f9da4c8bbb7c49e2a106a
Eswari Swathika.N <727724euai040@skcet.ac.in>
2025-07-09 18:37:55 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions=[
+{
+    numb:1,
+     question:" Who won the first individual Olympic gold medal for India?",
+    answer:"a) Abhinav Bindra",
+    options: [
+        "a) Abhinav Bindra",
+        "b) Milkha Singh",
+        "c) P.V. Sindhu",
+        "d) Leander Paes"
+    ]
+},
+{
+    numb:2,
+    question:"In which year did India host the first-ever FIFA U-17 World Cup?",
+    answer:"c) 2017",
+    options: [
+        "a) 2010",
+         "b) 2014",
+         "c) 2017",
+         "d) 2020"
+    ]
+},
+{
+    numb:3,
+    question:"Who is the first Indian woman to win an Olympic medal?",
+    answer:"b) Mary Kom",
+    options: [
+        "a) Saina Nehwal",
+        "b) Mary Kom",
+        "c) P.T. Usha",
+        "d) K.D. Jadhav"
+    ]
+},
+{
+    numb:4,
+    question:"Which Indian wrestler has won a gold medal at the World Wrestling Championships?",
+    answer:"d) Vinesh Phogat",
+    options: [
+        "a) Sushil Kumar",
+        "b) Yogeshwar Dutt",
+        "c) Bajrang Punia",
+        "d) Vinesh Phogat"
+    ]
+},
+{
+    numb:5,
+    question:"Who is the captain of the Indian National Football Team (as of 2025)?",
+    answer:"a) Sunil Chhetri",
+    options: [
+        "a) Sunil Chhetri",
+        "b) Bhaichung Bhutia",
+        "c) Gurpreet Singh Sandhu",
+        "d) Sandesh Jhingan"
+    ]
+}
+]
